343c5689a8873a04fde2d8cbc1fa38bc1d0cc1e4
chajancode <chajancode@gmail.com>
2026-05-23 15:42:46 +0400

    рефакторинг APIClient, PostsService

--- api/api_client.py
+++ api/api_client.py
@@ -1,65 +1,68 @@
-from typing import Optional
+from typing import Any, Optional
 
 from requests import Session
 from requests.auth import HTTPBasicAuth
 
 
 class APIClient:
-    def __init__(self, endpoint: str) -> None:
+    def __init__(self, endpoint: str, auth: HTTPBasicAuth) -> None:
         self.session = Session()
-        self.endpoint = endpoint
+        self.endpoint: str = endpoint
+        self.auth = auth
+
+    def _request(
+            self,
+            method: str,
+            url: str,
+            id: Optional[int] = None,
+            data: Optional[dict] = None,
+    ) -> dict[str, Any]:
+        if id:
+            url = f'{self.endpoint}/{id}'
+
+        response = self.session.request(
+            method=method,
+            url=url,
+            json=data,
+            auth=self.auth
+        )
+        return {
+            'status_code': response.status_code,
+            'response_body': response.json()
+        }
 
     def post(
         self,
         data: dict,
-        auth: Optional[HTTPBasicAuth] = None
     ) -> dict[str, dict]:
-        response = self.session.request(
+
+        return self._request(
             method='POST',
             url=self.endpoint,
-            params=data,
-            auth=auth
+            data=data
         )
-        response_content = {
-            'status_code': response.status_code,
-            'response_body': response.json()
-            }
-        return response_content
 
     def patch(
             self,
             id: int,
             data: dict,
-            auth: Optional[HTTPBasicAuth] = None
     ) -> dict[str, dict]:
-        url = f'{self.endpoint}/{id}'
-        response = self.session.request(
+
+        return self._request(
             method='PATCH',
-            url=url,
-            params=data,
-            auth=auth
+            url=self.endpoint,
+            id=id,
+            data=data
         )
-        response_content = {
-            'status_code': response.status_code,
-            'response_body': response.json()
-        }
-        return response_content
 
     def delete(
             self,
             id: int,
             data: dict,
-            auth: Optional[HTTPBasicAuth] = None
     ) -> dict[str, dict]:
-        url = f'{self.endpoint}/{id}'
-        response = self.session.request(
+        return self._request(
             method='DELETE',
-            url=url,
-            params=data,
-            auth=auth
+            url=self.endpoint,
+            id=id,
+            data=data
         )
-        response_content = {
-            'status_code': response.status_code,
-            'response_body': response.json()
-        }
-        return response_content
--- services/posts_service.py
+++ services/posts_service.py
@@ -5,18 +5,15 @@ from api.api_client import APIClient
 
 
 class PostsService:
+    def __init__(self, auth_data: dict) -> None:
+        self.auth = HTTPBasicAuth(**auth_data)
+        self.client = APIClient(wpe.POSTS_ENDPOINT, self.auth)
 
-    def create(self, test_data, auth_data):
-        basic_auth = HTTPBasicAuth(**auth_data)
-        client = APIClient(wpe.POSTS_ENDPOINT)
-        return client.post(test_data, basic_auth)
+    def create(self, test_data):
+        return self.client.post(test_data)
 
-    def patch(self, id, test_data, auth_data):
-        basic_auth = HTTPBasicAuth(**auth_data)
-        client = APIClient(wpe.POSTS_ENDPOINT)
-        return client.patch(id, test_data, basic_auth)
+    def patch(self, id, test_data):
+        return self.client.patch(id, test_data)
 
-    def delete(self, id, test_data, auth_data):
-        basic_auth = HTTPBasicAuth(**auth_data)
-        client = APIClient(wpe.POSTS_ENDPOINT)
-        return client.delete(id, test_data, basic_auth)
+    def delete(self, id, test_data):
+        return self.client.delete(id, test_data)
